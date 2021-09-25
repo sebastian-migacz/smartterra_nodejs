32966fedfb7b1367f322ae470b4606b184b59bcc
sebastian-migacz <vkensaj@gmail.com>
2021-09-25 14:44:55 +0200

    up

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 require('./utils')
+var mysql = require('mysql');
+//sql
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: "3306",
+    user: 'root',
+    password: 'tehheran',
+    database: 'world'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
+
+
 
 const express = require('express')
 const port = process.env.PORT || 3000
@@ -10,7 +31,7 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/kontakt', (req,res) => {
+app.get('/kontakt', (req, res) => {
     res.send('')
 })
 
